e2b55036886030ffc555b6cead1601a9a54871fd
Benoit Deschanel <benoit.deschanel@pj.ca>
2015-04-30 11:35:23 -0400

    Add comments & licenses

--- quicklib-android/src/main/java/com/quicklib/android/utils/LogUtils.java
+++ quicklib-android/src/main/java/com/quicklib/android/utils/LogUtils.java
@@ -1,12 +1,35 @@
+/*
+ * Copyright (C) 2015 Quicklib
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.quicklib.android.utils;
 
 import android.util.Log;
 
-import com.kysoprod.framework.BuildConfig;
+import com.quicklib.android.BuildConfig;
+import com.quicklib.android.core.Const;
 
+
+/**
+ * This class wrap Android's Log
+ *
+ * @author Benoit Deschanel
+ * @since 15-04-30
+ * Copyright (C) 2015 Quicklib
+ */
 public class LogUtils {
 
-    private static final String DEFAULT_TAG = "YPG";
     private static boolean debug = BuildConfig.DEBUG;
 
     public static String getStackTraceString(Throwable tr){
@@ -29,13 +52,13 @@ public class LogUtils {
 
     // DEBUG METHODS
     public static int d(String msg){
-        return d(DEFAULT_TAG, msg);
+        return d(Const.DEFAULT_TAG, msg);
     }
     public static int d(Throwable tr){
-        return d(DEFAULT_TAG, tr.getMessage(), tr);
+        return d(Const.DEFAULT_TAG, tr.getMessage(), tr);
     }
     public static int d(String msg, Throwable tr){
-        return d(DEFAULT_TAG, msg, tr);
+        return d(Const.DEFAULT_TAG, msg, tr);
     }
     public static int d(String tag, String msg){
         if(debug){
@@ -52,13 +75,13 @@ public class LogUtils {
 
     // ERROR METHODS
     public static int e(String msg){
-        return e(DEFAULT_TAG, msg);
+        return e(Const.DEFAULT_TAG, msg);
     }
     public static int e(Throwable tr){
-        return e(DEFAULT_TAG, tr.getMessage(), tr);
+        return e(Const.DEFAULT_TAG, tr.getMessage(), tr);
     }
     public static int e(String msg, Throwable tr){
-        return e(DEFAULT_TAG, msg, tr);
+        return e(Const.DEFAULT_TAG, msg, tr);
     }
     public static int e(String tag, String msg){
         if(debug){
@@ -75,13 +98,13 @@ public class LogUtils {
 
     // INFO METHODS
     public static int i(String msg){
-        return i(DEFAULT_TAG, msg);
+        return i(Const.DEFAULT_TAG, msg);
     }
     public static int i(Throwable tr){
-        return i(DEFAULT_TAG, tr.getMessage(), tr);
+        return i(Const.DEFAULT_TAG, tr.getMessage(), tr);
     }
     public static int i(String msg, Throwable tr){
-        return i(DEFAULT_TAG, msg, tr);
+        return i(Const.DEFAULT_TAG, msg, tr);
     }
     public static int i(String tag, String msg){
         if(debug){
@@ -98,13 +121,13 @@ public class LogUtils {
 
     // VERBOSE METHODS
     public static int v(String msg){
-        return v(DEFAULT_TAG, msg);
+        return v(Const.DEFAULT_TAG, msg);
     }
     public static int v(Throwable tr){
-        return v(DEFAULT_TAG, tr.getMessage(), tr);
+        return v(Const.DEFAULT_TAG, tr.getMessage(), tr);
     }
     public static int v(String msg, Throwable tr){
-        return v(DEFAULT_TAG, msg, tr);
+        return v(Const.DEFAULT_TAG, msg, tr);
     }
     public static int v(String tag, String msg, Throwable tr){
         if(debug){
@@ -121,13 +144,13 @@ public class LogUtils {
 
     // WARNING METHODS
     public static int w(  String msg){
-        return w(DEFAULT_TAG, msg);
+        return w(Const.DEFAULT_TAG, msg);
     }
     public static int w(Throwable tr){
-        return w(DEFAULT_TAG, tr.getMessage(), tr);
+        return w(Const.DEFAULT_TAG, tr.getMessage(), tr);
     }
     public static int w(String msg, Throwable tr){
-        return w(DEFAULT_TAG, msg, tr);
+        return w(Const.DEFAULT_TAG, msg, tr);
     }
     public static int w(String tag, String msg){
         if(debug){
@@ -144,13 +167,13 @@ public class LogUtils {
 
     // WHAT A TERRIBLE FAILURE METHODS (or "What The Fuck" Methods)
     public static int wtf(  String msg){
-        return wtf(DEFAULT_TAG, msg);
+        return wtf(Const.DEFAULT_TAG, msg);
     }
     public static int wtf(Throwable tr){
-        return e(DEFAULT_TAG, tr);
+        return e(Const.DEFAULT_TAG, tr);
     }
     public static int wtf(String msg, Throwable tr){
-        return wtf(DEFAULT_TAG, msg, tr);
+        return wtf(Const.DEFAULT_TAG, msg, tr);
     }
     public static int wtf(String tag, String msg){
         if(debug){
--- quicklib-android/src/test/java/com/quicklib/android/utils/StringUtilsTest.java
+++ quicklib-android/src/test/java/com/quicklib/android/utils/StringUtilsTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 Quicklib
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.quicklib.android.utils;
 
 import org.junit.Before;
@@ -9,7 +24,9 @@ import java.io.InputStream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
- * Created by benoit on 15-04-25.
+ * @author Benoit Deschanel
+ * @since 15-04-30
+ * Copyright (C) 2015 Quicklib
  */
 public class StringUtilsTest {
 
