d367e8afa3a7c380acaf441c9516979723d6ff77
Benoit D <kyso84@gmail.com>
2018-11-22 21:10:43 -0500

    Add lifecycleowner in QuickViewHolder

--- quicklib-mvvm/src/main/java/com/quicklib/android/mvvm/view/QuickViewHolder.kt
+++ quicklib-mvvm/src/main/java/com/quicklib/android/mvvm/view/QuickViewHolder.kt
@@ -6,17 +6,20 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
 import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.quicklib.android.mvvm.QuickView
 
 abstract class QuickViewHolder<T, VDB : ViewDataBinding, VM : ViewModel>(protected val binding: VDB) : RecyclerView.ViewHolder(binding.root), QuickView<VDB, VM> {
-
     constructor(inflater: LayoutInflater, container: ViewGroup?, @LayoutRes layoutId: Int) : this(DataBindingUtil.inflate(inflater, layoutId, container, false))
     constructor(context: Context, container: ViewGroup?, @LayoutRes layoutId: Int) : this(DataBindingUtil.inflate(LayoutInflater.from(context), layoutId, container, false))
 
     protected val viewModel = getViewModelInstance()
+    protected val lifeCycleOwner: LifecycleOwner?
+        get() = if (itemView.context is LifecycleOwner) itemView.context as LifecycleOwner else null
+
 
     init {
         onBindingReady(binding)
