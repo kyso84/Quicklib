7f84916a7a52eeb91de7bb2fe6f0f2c0dd7fb861
Benoit Deschanel <kyso84@gmail.com>
2018-11-23 13:56:01 -0500

    New viewHolder constructor

--- quicklib-mvvm/src/main/java/com/quicklib/android/mvvm/view/QuickViewHolder.kt
+++ quicklib-mvvm/src/main/java/com/quicklib/android/mvvm/view/QuickViewHolder.kt
@@ -5,21 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.recyclerview.widget.RecyclerView
 import com.quicklib.android.mvvm.QuickView
 
-abstract class QuickViewHolder<T, VDB : ViewDataBinding, VM : ViewModel>(protected val binding: VDB) : RecyclerView.ViewHolder(binding.root), QuickView<VDB, VM> {
-    constructor(inflater: LayoutInflater, container: ViewGroup?, @LayoutRes layoutId: Int) : this(DataBindingUtil.inflate(inflater, layoutId, container, false))
-    constructor(context: Context, container: ViewGroup?, @LayoutRes layoutId: Int) : this(DataBindingUtil.inflate(LayoutInflater.from(context), layoutId, container, false))
+abstract class QuickViewHolder<T, VDB : ViewDataBinding, VM : ViewModel>(protected val lifecycleOwner: LifecycleOwner, protected val binding: VDB) : RecyclerView.ViewHolder(binding.root), QuickView<VDB, VM> {
+    constructor(appCompatActivity: AppCompatActivity, binding: VDB) : this(appCompatActivity as LifecycleOwner, binding)
+    constructor(fragment: Fragment, binding: VDB) : this(fragment as LifecycleOwner, binding)
 
     protected val viewModel = getViewModelInstance()
-    protected val lifeCycleOwner: LifecycleOwner?
-        get() = if (itemView.context is LifecycleOwner) itemView.context as LifecycleOwner else null
-
 
     init {
         onBindingReady(binding)
